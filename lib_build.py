55d0ae21bb2a64217e70839810910b168a7f9c4b
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2015-12-01 14:45:24 +1100

    Library building functionality (A simple copy)

---
+++ lib_build.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from os import path
+from shutil import copy2 as copy
+import sys
+
+def libfile(sourcedir, libname):
+    lfname = path.join(sourcedir, libname + '.pcl')
+    if not path.isfile(lfname):
+        raise NameError('library module not found: ' + libname)
+    return lfname
+
+if __name__=='__main__':
+    source = sys.argv[1]
+    dest = sys.argv[2]
+    utilsets = sys.argv[3:]
+    for lib in utilsets:
+        copy(libfile(source, lib), dest)
